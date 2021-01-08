6b04dcc219a20aa5ff4e758ef846c4ed6cba4912
Tayden Louw <tayden@TAYDENLMBP.local>
2021-01-08 15:39:36 +0200

    Fixed broken webpack imports

--- apps/micro-app1/webpack.config.js
+++ apps/micro-app1/webpack.config.js
@@ -1,6 +1,5 @@
 const singleSpaAngularWebpack = require('single-spa-angular/lib/webpack')
   .default;
-
 module.exports = (angularWebpackConfig, options) => {
   const singleSpaWebpackConfig = singleSpaAngularWebpack(
     angularWebpackConfig,
@@ -9,20 +8,38 @@ module.exports = (angularWebpackConfig, options) => {
 
   singleSpaWebpackConfig.output.library = 'microApp1';
 
+  const mappings = {
+    '@angular/core': 'core',
+    '@angular/common': 'common',
+    '@angular/common/http': 'common.http',
+    '@angular/platform-browser': 'platformBrowser',
+    '@angular/platform-browser-dynamic': 'platformBrowserDynamic',
+    '@angular/compiler': 'compiler',
+    '@angular/animations': 'animations',
+  };
+
+  const angularExternals = Object.keys(mappings).reduce(
+    (accumulator, mapping) => {
+      const request = ['ng', mappings[mapping]];
+
+      accumulator[mapping] = {
+        root: request,
+        commonjs: request,
+        commonjs2: request,
+        amd: request,
+      };
+
+      return accumulator;
+    }
+  );
+
   return {
     ...singleSpaWebpackConfig,
     externals: {
       rxjs: 'rxjs',
-      '@angular/core': 'ng.core',
-      '@angular/common': 'ng.common',
-      '@angular/common/http': 'ng.common.http',
-      '@angular/platform-browser': 'ng.platformBrowser',
-      '@angular/platform-browser-dynamic': 'ng.platformBrowserDynamic',
-      '@angular/compiler': 'ng.compiler',
-      '@angular/animations': 'ng.animations',
-      // '@angular/router': 'ng.router',
-      // '@angular/forms': 'ng.forms',
+      ...angularExternals,
       'single-spa-angular': 'single-spa-angular',
+      'single-spa-single-spa-angular-internals': 'single-spa-angular-internals',
     },
   };
 };
